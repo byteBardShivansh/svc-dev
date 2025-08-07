079c120bcaf446b7222215405633b9783f64f4d5
Shivansh Mishra <autocodebuddy@gmail.com>
2025-08-07 21:23:57 +0530

    changed main

--- main.tf
+++ main.tf
@@ -23,7 +23,11 @@ resource "random_string" "suffix" {
 }
 
 resource "google_storage_bucket" "default" {
-  name     = "${var.base_bucket_name}-${random_string.suffix.result}"
-  location = "US"
+  name          = "${var.base_bucket_name}-${random_string.suffix.result}"
+  location      = "US"
   force_destroy = true
+
+  versioning {
+    enabled = true
+  }
 }
