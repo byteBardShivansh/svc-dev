590b0c0380b2de68844bf54855fb529a3ea56782
Shivansh Mishra <autocodebuddy@gmail.com>
2025-08-20 19:01:16 +0530

    cutom-bucket-name

--- variables.tf
+++ variables.tf
@@ -1,5 +1,4 @@
 variable "base_bucket_name" {
   description = "The base name for the Google Cloud Storage bucket. A random suffix will be appended."
   type        = string
-  default     = "web-storage"
 }
